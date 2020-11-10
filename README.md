458ed3a69c22fccf14342a2f3a5d0e075ef0f096
ChenEating716 <390213346@qq.com>
2020-11-11 00:46:26 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ur5e-robotiq_85-in-pybullet
+
+竟然在网上没有找到带有二指爪的UR5e，自己加了一个上去。
---
+++ test.py
@@ -0,0 +1,28 @@
+
+import pybullet as p
+from time import sleep
+import pybullet_data
+
+
+physicsClient = p.connect(p.GUI)
+
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+p.setGravity(0, 0, -10)
+planeId = p.loadURDF("plane.urdf")
+cubeStartPos = [0, 0, 1]
+cubeStartOrientation = p.getQuaternionFromEuler([0, 0, 0])
+boxId = p.loadURDF("./ur_e_description/urdf/ur5e_robotiq85.urdf", cubeStartPos, cubeStartOrientation)
+cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
+
+useRealTimeSimulation = 0
+
+if (useRealTimeSimulation):
+  p.setRealTimeSimulation(1)
+
+while 1:
+  if (useRealTimeSimulation):
+    p.setGravity(0, 0, -10)
+    sleep(0.01)  # Time in seconds.
+  else:
+    p.stepSimulation()
